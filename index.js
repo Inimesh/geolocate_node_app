7fb8e2b4ff46b63a154f4d4e632f2f4359771c0d
inimesh <ijmeshoulam@gmail.com>
2022-03-22 15:54:38 +0000

    Adding ability to send geolocation data to the server and send client back a response object. Also installing nodemon dependency.

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const port = 3000;
 app.listen(port, () => { console.log("Server listening on port " + port) });
 
 app.use(express.static('public'));
+app.use(express.json());
+
+
+app.post('/api', (req, res) => {
+  console.log(req.body);
+  const res_data = req.body;
+  res.json({
+    status: "success",
+    latitude: res_data.lat,
+    longitude: res_data.lon
+  })
+})
