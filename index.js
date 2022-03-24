3a02d9b92b47fc15eca9b36ab9aeb2d0698afc7a
inimesh <ijmeshoulam@gmail.com>
2022-03-24 14:53:09 +0000

    Successful response of all database entries on GET request to '/api' endpoint

--- index.js
+++ index.js
@@ -14,12 +14,25 @@ const database = new Datastore('database.db');
 database.loadDatabase();
 
 // GET Requests --------------------------------------------------
+
+// Returning environment key for mapbox tiling
 app.get('/mapboxkey', (req, res) => {
   res.json({
     key: process.env.MAPBOX_API_KEY
   });
 });
 
+// querying database for all entries and sending back in response
+app.get('/api', (req, res) => {
+  database.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+      response.end();
+      return;
+    }
+    res.json({ data });
+  });
+});
 // POST Requests --------------------------------------------------
 app.post('/api', (req, res) => {
   const data = req.body;
